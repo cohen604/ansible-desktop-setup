125df56354152e1aea9982205090454c90d857eb
Gabriel <26357094+cohen604@users.noreply.github.com>
2021-08-08 19:53:16 +0300

    fixed nvim config error

--- roles/nvim/files/init.lua
+++ roles/nvim/files/init.lua
@@ -1,11 +1,14 @@
+local g = vim.g
+local cmd = vim.cmd
+
 cmd [[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]]
 cmd [[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]]
 cmd [[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]]
 cmd [[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]]
 cmd [[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]]
 
-vim.g.tokyodark_transparent_background = false
-vim.g.tokyodark_enable_italic_comment = true
-vim.g.tokyodark_enable_italic = true
-vim.g.tokyodark_color_gamma = "1.0"
-vim.cmd("colorscheme tokyodark")
+g.tokyodark_transparent_background = false
+g.tokyodark_enable_italic_comment = true
+g.tokyodark_enable_italic = true
+g.tokyodark_color_gamma = "1.0"
+cmd [[colorscheme tokyodark]]
